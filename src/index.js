e1f35dc5147bccdf3f4be62712b65274f4808b65
KAMILAGOIT <152889939+KAMILAGOIT@users.noreply.github.com>
2024-03-15 19:20:56 +0100

    1*

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchCountries } from './fetchCountries/fetchCountries.js';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
