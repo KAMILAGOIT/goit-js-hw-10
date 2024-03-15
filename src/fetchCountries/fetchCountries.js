9a084b6b044e14f115a4b42a41fc7ba674c2cc29
KAMILAGOIT <152889939+KAMILAGOIT@users.noreply.github.com>
2024-03-15 21:49:51 +0100

    Update fetchCountries.js

--- src/fetchCountries/fetchCountries.js
+++ src/fetchCountries/fetchCountries.js
@@ -1,5 +1,5 @@
 export async function fetchCountries(name) {
-  const response = await fetch(`https://restcountries.com/v3.1/all?fields=name,flag`);
+  const response = await fetch(`https://restcountries.com/v3.1/name`);
   if (!response.ok) {
     throw new Error('Failed to fetch countries');
   }
