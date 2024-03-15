cf8c34f2f9515948bf6da87285e94f7c644f22e7
KAMILAGOIT <152889939+KAMILAGOIT@users.noreply.github.com>
2024-03-15 19:26:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchCountries } from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries/fetchCountries';
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
