5cfd4d4bd84ad90ba247760a69d6fe26824212bb
KAMILAGOIT <152889939+KAMILAGOIT@users.noreply.github.com>
2024-03-15 18:51:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -97,17 +97,7 @@ function clearResults() {
 }
 
 
-function debounce(func, delay) {
-    let timeoutId;
-    return function(...args) {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
-}
+
 //const debounce = require('lodash.debounce');//
 
 //function alertWrongName() {
