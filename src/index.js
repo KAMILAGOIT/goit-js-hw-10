23f2c2b68058afebe5a3da12119ffe5676d8c7e2
KAMILAGOIT <152889939+KAMILAGOIT@users.noreply.github.com>
2024-03-15 22:45:54 +0100

    5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css'
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix'
-import { fetchCountries } from './js/fetch-countries'
+import { fetchCountries } from './fetchCountries/fetchCountries'
 
 const DEBOUNCE_DELAY = 300
 
